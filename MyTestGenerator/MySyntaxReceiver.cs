30057dfe877cbe6d2c18a72c1985ed499076c948
Matt Lacey <matt@mrlacey.co.uk>
2022-02-08 10:50:23 +0000

    repro

---
+++ MyTestGenerator/MySyntaxReceiver.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MyTestGenerator
+{
+    public class MySyntaxReceiver : ISyntaxContextReceiver
+    {
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            // Do nothing here.
+            // This just exists to simplify
+        }
+    }
+}
+
+
+
+
---
+++ SourceGenOfTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourceGenOfTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ThisIsTestWasWrittenByADeveloper()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
